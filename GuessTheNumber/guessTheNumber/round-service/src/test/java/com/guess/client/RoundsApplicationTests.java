48dd836216bd3b030d8b673ae9d9f684dd336114
rosielleny <r-e-young@outlook.com>
2023-08-07 11:43:47 +0100

    GuessTheNumber

---
+++ GuessTheNumber/guessTheNumber/round-service/src/test/java/com/guess/client/RoundsApplicationTests.java
@@ -0,0 +1,60 @@
+package com.guess.client;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import com.guess.dto.entity.Round;
+import com.guess.model.persistence.RoundDaoImpl;
+
+@SpringBootTest
+@ActiveProfiles("test")
+class RoundsApplicationTests {
+	
+	@Autowired
+	private RoundDaoImpl roundDao;
+
+	@Test
+    @DisplayName("Get A List Of All Rounds")
+    public void getListOfAllStudentsTest() {
+        List<Round> newList = roundDao.getAllRounds(1);
+        assertNotNull(newList);
+        assertEquals(3, newList.size());
+    }
+ 
+   @Test
+    @DisplayName("Find Single Round By Id")
+    public void findStudentByIdTest() {
+        Round round = roundDao.getSingleRound(1);
+        assertNotNull(round);
+        assertEquals("4321", round.getGuess());
+        assertEquals("e:0:p:4", round.getResult());
+    }
+   
+   @Test
+   @DisplayName("Add a New Round")
+   void addNewRoundTest() {
+       
+       Round round = new Round();
+       round.setGameId(1);
+       round.setGuess("1234");
+       round.setResult("e:2:p:1");
+       round.setTimeOfGuess(LocalDateTime.now());
+
+       
+       Round addedRound = roundDao.addNewRound(round);
+
+       
+       assertNotNull(addedRound);
+       
+   }
+
+}
